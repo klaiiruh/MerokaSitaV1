e5e65ff24b1bdd51c79e2668dd154fe65887b8d8
Clara Caden <clara.caden@mail.mcgill.ca>
2026-02-04 17:21:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MerokaWebsite from './App.jsx' // Make sure your original code file is named App.jsx
-import './index.css'
+import App from './App.jsx' // This points to your code
+import './index.css'     // This imports your Tailwind styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MerokaWebsite />
+    <App />
   </React.StrictMode>,
 )
