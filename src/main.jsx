19094bcd2bd40443b9eade2f75b5abe891d6858b
Clara Caden <clara.caden@mail.mcgill.ca>
2026-02-04 17:18:49 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MerokaWebsite from './App.jsx' // Make sure your original code file is named App.jsx
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MerokaWebsite />
+  </React.StrictMode>,
+)
